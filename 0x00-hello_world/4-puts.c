d7e9b6edf0d8f64626ad4df2fb5f503e5b09ad82
Walton Lee <walton.lee@holbertonschool.com>
2016-10-12 23:35:52 +0000

    commenting out C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-main(void)
+/**
+ * main - This code prints out the text below that is encased in the puts command.
+ * Return: The value returns 0.
+ */
+int main(void)
 {
 	puts("\"Programming is like builiding a multilingual puzzle");
 	return (0);

40854dea78f9df46ffc5d008ceeb267ae6de6287
Walton Lee <walton.lee@holbertonschool.com>
2016-10-13 20:39:32 +0000

    Printing the last digit of a big number and based off of the last digit run a conditional statement on it. Revision 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n % 10 < 6 && n != 0)
+	if (n % 10 < 6 && n % 10 != 0)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n",
 		       n, n % 10);
 	else if (n % 10 <= 0 && n % 10 >= 0)

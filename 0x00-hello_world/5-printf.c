781f924416c4c04bca6bb0b980fb301cce0c8200
Walton Lee <walton.lee@holbertonschool.com>
2016-10-13 00:08:17 +0000

    Using printf to print text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Using printf to print out text
+ * Return: The value 0 will be returned
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

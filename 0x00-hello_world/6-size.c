2992bf9e3e3dd981976046065a8fd98740921cc9
Walton Lee <walton.lee@holbertonschool.com>
2016-10-13 02:33:26 +0000

    Properly tabbing the variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,13 @@
  *Return: The output returns 0.
  */
 int main(void)
+
 {
-char c;
-int i;
-long int li;
-long long int lli;
-float f;
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
 
 	printf("Size of a char:%d byte(s)\n", sizeof(c));
 	printf("Size of an int:%d byte(s)\n", sizeof(i));

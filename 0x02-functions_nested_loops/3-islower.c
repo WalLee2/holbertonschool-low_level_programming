324e435ca9065aa8c0de0d41ccfd2f2bfcfb237d
Walton Lee <walton.lee@holbertonschool.com>
2016-10-18 16:46:20 +0000

    changing the comments

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 /**
+ *_islower - checks for lowercase characters.
+ *
+ *@c: evaluates the acii value of the letter and gets it through the conditional
+ *
  *main - writing a function that checks for lowercase characters.
- *Description: checking for lowercase characters.
+ *
  *Return: 1 if c is lowercase, 0 for other cases.
  */
 

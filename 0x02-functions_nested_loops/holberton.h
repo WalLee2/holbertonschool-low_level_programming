608aae30d42a979766fb417affca7686bed92e49
Walton Lee <walton.lee@holbertonschool.com>
2016-10-18 22:11:31 +0000

    updating library

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
+int main(void);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -6,3 +7,4 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int x);
 int print_last_digit(int r);
+void jack_bauer(void);

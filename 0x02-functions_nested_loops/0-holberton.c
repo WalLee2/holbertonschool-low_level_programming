b8531fb1507082377274e54bead3b279c581fd2d
Walton Lee <walton.lee@holbertonschool.com>
2016-10-18 19:10:23 +0000

    printing out Holberton followed by a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 /**
  *main - writing a program that prints Holberton followed by a new line.
  *Return: The value 0 is returned.
@@ -6,9 +7,15 @@
 int main(void)
 
 {
-	char H = 'H', o = 'o', l = 'l', b = 'b', e = 'e', r = 'r', t = 't';
-	char o = 'o', n = 'n';
-
-	_putchar('c');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }

25a0c6eefe97a5ed0e391c332a93f8667e6ee038
Walton Lee <walton.lee@holbertonschool.com>
2016-10-18 00:37:17 +0000

    printing out the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+/**
+ *main - writing a program that prints Holberton followed by a new line.
+ *Return: The value 0 is returned.
+ */
+
+int main(void)
+
+{
+	char H = 'H', o = 'o', l = 'l', b = 'b', e = 'e', r = 'r', t = 't';
+	char o = 'o', n = 'n';
+
+	_putchar('c');
+	return (0);
+}

04e4f53cde84d7104f78ecb79791e25cd1aa74fb
Walton Lee <walton.lee@holbertonschool.com>
2016-10-18 00:32:49 +0000

    printing the alphabet in lowercase characters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - printing out the alphabet
+ *Return: Always 0.
+ */
+
+void print_alphabet(void)
+
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

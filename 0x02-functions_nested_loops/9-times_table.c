4093a6c3fdcdc659d87a03fc9843d8eea051add1
Walton Lee <walton.lee@holbertonschool.com>
2016-10-19 01:39:56 +0000

    correctly commenting out code and checking for semantic errors.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,32 +2,37 @@
 
 /**
  *times_table - printing the 9 times table starting with 0
- *Description: Times table.
+ *
  *Return: The return value is void.
  */
-void skeleton(int n);
 void times_table(void)
+
 {
 	int i, k, l;
+
 	l = 1;
 
-	while ( l <= 9 )
+	while (l <= 9)
 	{
 		i = 0;
 		k = 1;
 		_putchar('0');
-		while ( i < 9 )
+		while (i < 9)
 		{
-			skeleton( l * k );
+			skeleton(l * k);
 			i++;
-		       	k++;
+			k++;
 		}
 		l++;
 		_putchar('\n');
 	}
 }
 
-
+/**
+ *skeleton - reused to produce a comma and space(s) between
+ *each number in the times table.
+ *@n: the parameter n keeps track of when to put commas, spaces and the number
+ */
 void skeleton(int n)
 {
 	if (n <= 9)

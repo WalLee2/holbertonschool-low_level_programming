2b170dff08bbb5e80c98d706d7871c4a0a57adc9
Walton Lee <walton.lee@holbertonschool.com>
2016-10-25 02:05:33 +0000

    Writing a function that takes a pointer to an int as parameter.

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - writing a function that takes a pointer to an int as a
+ *parameter and updates the value it points to.
+ *
+ *@n: a pointer variable
+ *
+ *Return: The value returns 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

4119154c8f95bd3dba46122f24fd09ba19af1570
Walton Lee <walton.lee@holbertonschool.com>
2016-10-25 18:53:57 +0000

    Printing a string in reverse.

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *print_rev - Printing out a string in reverse.
+ *@s: Variable that holds the string.
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	for ( ; s[i] != (s[0] && s[1]); i--)
+	{
+		_putchar(s[i - 1]);
+	}
+	_putchar('\n');
+}

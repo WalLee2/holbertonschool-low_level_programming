878113851908da2185efee6965482c5a609b9c5b
Walton Lee <walton.lee@holbertonschool.com>
2016-10-26 01:53:18 +0000

    printing half of the string

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -9,22 +9,10 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 1; str[i] != '\0'; i++)
 		;
-	if (i % 2 == 0)
-	{
-		for (i = i / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = (i - 1 / 2); str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-		_putchar('\n');
-	}
+	i++;
+	for (i = i / 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }

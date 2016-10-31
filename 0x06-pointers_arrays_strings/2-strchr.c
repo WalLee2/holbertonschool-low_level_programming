de4fbe314f39bc46fd2876bca7bee04696510b2f
Walton Lee <walton.lee@holbertonschool.com>
2016-10-31 19:39:04 +0000

    A function that locates a character in a string.

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_strchr - a function that finds the first occurance of that letter
+ *in that string.
+ *@s:Takes the value of the string.
+ *@c: The character that you want to find in the string.
+ *Return: the characters that you wanted in the string.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			break;
+		}
+		*s++;
+	}
+	return (s);
+}

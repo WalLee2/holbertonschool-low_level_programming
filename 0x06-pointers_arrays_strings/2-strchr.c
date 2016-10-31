f0522de61d02b9ee79b86a66068a4a11ea3ee137
Walton Lee <walton.lee@holbertonschool.com>
2016-10-31 20:34:09 +0000

    accounting for edge case.

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -4,17 +4,18 @@
  *in that string.
  *@s:Takes the value of the string.
  *@c: The character that you want to find in the string.
- *Return: the characters that you wanted in the string.
+ *Return: the characters that you wanted in the string or Null
+ *if they don't exist.
  */
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == c)
+		while (*s == c)
 		{
-			break;
+			return (s);
 		}
-		*s++;
+		s++;
 	}
-	return (s);
+	return ("Null");
 }

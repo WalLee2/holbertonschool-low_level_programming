98e2eeb59a483ed5a10fea8bd98ac7a85353a24b
Walton Lee <walton.lee@holbertonschool.com>
2016-11-01 05:34:10 +0000

    printing a chessboard.

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, i2;
+
+	i = 0;
+	while (i < 8)
+	{
+		i2 = 0;
+		while (i2 < 8)
+		{
+			_putchar(a[i][i2]);
+			i2++;
+		}
+		i++;
+		_putchar('\n');
+	}
+	_putchar('\n');
+}

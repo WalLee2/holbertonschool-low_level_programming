591f14edfe289acbe090329635909b0dfd8f0e8c
Walton Lee <walton.lee@holbertonschool.com>
2016-11-05 01:33:16 +0000

    Writing a program that prints the number of arguments you passed to it.

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ *main - A function that prints out the number of arguments that are passed
+ *to it.
+ *@argc: Variable that takes in the arguments.
+ *@argv: A variable that takes in the argument as a string.
+ *Return: The value returned is 0.
  */
 int main(int argc, char *argv[])
 {

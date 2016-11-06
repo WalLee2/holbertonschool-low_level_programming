310a2945cb15e8334c6ab776819931ac5b7c5a09
Walton Lee <walton.lee@holbertonschool.com>
2016-11-06 21:04:37 +0000

    multiplying two numbers.

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Multiplies two numbers
+ *@argc: A variable that counts the number of arguments it takes in
+ *@argv: A variable that turns the arguments into strings.
+ *Return: The value 0 is returned.
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argv[1] && argv[2])
+	{
+		result = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", result);
+	}
+	return (0);
+}

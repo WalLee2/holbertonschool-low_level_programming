31f3cf373940a4f66d0cf6fc69e9c230d9bf943a
Walton Lee <walton.lee@holbertonschool.com>
2016-11-09 18:45:34 +0000

    Accounting for an edge case.

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -12,8 +12,10 @@ char *str_concat(char *s1, char *s2)
 	int i, a;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
-		return ("");
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (a = 0; s2[a] != '\0'; a++)
@@ -32,8 +34,5 @@ char *str_concat(char *s1, char *s2)
 		p[i] = s2[a];
 		i++;
 	p[i] = '\0';
-	if (p == NULL)
-		return (NULL);
-	else
-		return (p);
+	return (p);
 }

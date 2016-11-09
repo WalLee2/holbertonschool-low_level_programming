05c316082069c9540e8879bee5037ef74d5a5c7d
Walton Lee <walton.lee@holbertonschool.com>
2016-11-09 03:41:04 +0000

    A function that concatenates all the arguments.

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *argstostr - A function that concatenates all the arguments of the program
+ *@ac: The number of arguments
+ *@av: The characters being passed
+ *Return: The arguments being passed through
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, a;
+	char *temp, *r;
+
+	r = (char *)malloc(ac * sizeof(char *));
+	temp = r;
+	for (i = 0; i < ac; i++)
+	{
+		a = 0;
+		while (av[i][a] != '\0')
+		{
+			*r = av[i][a];
+			a++;
+			r++;
+		}
+		*r = '\n';
+		r++;
+	}
+	if (temp)
+	{
+		return (t);
+	}
+	else
+	{
+		return (NULL);
+	}
+
+}

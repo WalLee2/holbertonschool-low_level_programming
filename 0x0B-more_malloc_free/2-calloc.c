98debe5cc4c662151e71cbcd54ca9247e19f9899
Walton Lee <mr.waltonlee@me.com>
2016-11-10 22:52:48 +0000

    Setting the memory to 0

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -8,12 +8,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *p;
+	unsigned int i, *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * sizeof(size));
 	if (p == NULL)
 		return (NULL);
+	i = 0;
+	while (i < nmemb)
+	{
+		p[i] = 0;
+		i++;
+	}
 	return (p);
 }

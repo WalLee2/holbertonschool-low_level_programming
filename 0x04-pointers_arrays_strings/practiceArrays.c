6e99f75caae01d81d481961eeda11ba1eddc745e
Walton Lee <mr.waltonlee@me.com>
2016-11-14 18:06:58 +0000

    adding all main.c files in 0x04

---
+++ 0x04-pointers_arrays_strings/practiceArrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - using sizeof to dynamically determine the size of type char,
+ *int and float
+ *
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+	printf("Size of type 'char' on my computer: %lu b", ptr );
+	return (0);
+}

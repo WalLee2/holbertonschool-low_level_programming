8fb0f39df33e6521f22ccae637346c5687cbce1c
Walton Lee <mr.waltonlee@me.com>
2016-11-14 22:17:59 +0000

    Defining a structure

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - a structure that creates variables used in the main function
+ *which is also accessed in the main function
+ *@name: A pointer pointing to a string
+ *@age: A float that takes in an a number
+ *@owner: A pointer variable that points to a string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

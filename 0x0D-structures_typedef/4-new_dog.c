4d6f2588d9d5f6c2d14751682a905a90582eb3bb
Walton Lee <mr.waltonlee@me.com>
2016-11-15 07:18:14 +0000

    accounting for edge cases and appending the null character at the end of the string

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,15 +1,22 @@
 #include "dog.h"
 #include <stdlib.h>
-
+/**
+ *new_dog - accounts for the length of the input, allocates memory
+ *to a new space, prints the input in the space
+ *@name: pointer variable that points to a string
+ *@age: float variable that takes in a number
+ *@owner: pointer variable that points to a string
+ *Return: input values given
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, lenN, lenO;
 	dog_t *doggy;
 
-	for(i = 0; name[i] != '\0'; i++)
+	for (i = 0; name[i] != '\0'; i++)
 		lenN++;
-	for(i = 0; owner[i] != '\0'; i++)
+	for (i = 0; owner[i] != '\0'; i++)
 		lenO++;
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
@@ -23,7 +30,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	for (i = 0; name[i] != '\0'; i++)
 		doggy->name[i] = name[i];
+	doggy->name[i] = '\0';
 	for (i = 0; owner[i] != '\0'; i++)
 		doggy->owner[i] = owner[i];
+	doggy->owner[i] = '\0';
 	return (doggy);
 }

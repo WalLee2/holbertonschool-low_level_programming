0bcd4ce18b05da8cc747a3dc7973ac4c0ef3e46c
Walton Lee <mr.waltonlee@me.com>
2016-11-15 06:59:59 +0000

    refactoring code

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,39 +1,29 @@
 #include "dog.h"
 #include <stdlib.h>
-/**
- *new_dog - creating a new dog
- *@name: Name of the dog
- *@age: a float value
- *@owner: a pointer that points to a string
- *Return: The name, age, and owner is returned
- */
+
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, lenN, lenO;
 	dog_t *doggy;
-	int i;
-	char *temp;
 
+	for(i = 0; name[i] != '\0'; i++)
+		lenN++;
+	for(i = 0; owner[i] != '\0'; i++)
+		lenO++;
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 		return (NULL);
-	temp = name;
-	for (i = 0; *temp != '\0'; i++, temp++)
-		;
-	if (i == 0)
-		return (NULL);
-	doggy->name = malloc(sizeof(char) * i);
+	doggy->name = malloc(sizeof(lenN));
 	if (doggy->name == NULL)
 		return (NULL);
-	doggy->name = name;
 	doggy->age = age;
-	temp = owner;
-	for (i = 0; *temp != '\0'; i++, temp++)
-		;
-	if (i == 0)
-		return (NULL);
-	doggy->owner = malloc(sizeof(char) * i);
+	doggy->owner = malloc(sizeof(lenO));
 	if (doggy->owner == NULL)
 		return (NULL);
-	doggy->owner = owner;
+	for (i = 0; name[i] != '\0'; i++)
+		doggy->name[i] = name[i];
+	for (i = 0; owner[i] != '\0'; i++)
+		doggy->owner[i] = owner[i];
 	return (doggy);
 }

70d800028787dbd847575b1e462ddabbefd938f9
Walton Lee <mr.waltonlee@me.com>
2016-11-20 00:23:04 +0000

    fixing comments and styling

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,31 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ *print_string - prints a string
+ *@param: list of parameters
+ *@separator: passing in a separator
  */
 void print_string(va_list param, char *separator)
 {
 	char *str;
+
 	str = va_arg(param, char *);
 	if (str == NULL)
 		str = "(nil)";
 	printf("%s%s", separator, str);
 }
+/**
+ *print_int - prints an int
+ *@param: list of parameters
+ *@separator: passing in a separator
+ */
 void print_int(va_list param, char *separator)
 {
 	printf("%s%d", separator, va_arg(param, int));
 }
+/**
+ *print_float - prints a float
+ *@param: list of paramters
+ *@separator: passing in a separator
+ */
 void print_float(va_list param, char *separator)
 {
 	printf("%s%f", separator, va_arg(param, double));
 }
+/**
+ *print_char -  prints a character
+ *@param: list of parameters
+ *@separator: passing in a separator
+ */
 void print_char(va_list param, char *separator)
 {
 	printf("%s%c", separator, va_arg(param, int));
 }
-
+/**
+ *print_all - evalutates each parameter and
+ *passes them to the appropriate function
+ *@format: a string
+ */
 void print_all(const char * const format, ...)
 {
 	print_a_t print_a[] = {

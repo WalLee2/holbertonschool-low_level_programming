86ce8261f442b58340a78f664fa7db28e9bfc6cb
Walton Lee <mr.waltonlee@me.com>
2016-11-29 03:28:49 +0000

    adding comments and pushing header file.

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 #include <string.h>
 /**
- *
- *
- *
- *
+ *add_node_end - a function that adds a node at the end of the list
+ *@head: a double pointer taking the address of what's being passed to it
+ *@str: the string to be copied
+ *Return: The result of the new_node which is the list
  */
 list_t *add_node_end(list_t **head, const char *str)
 {

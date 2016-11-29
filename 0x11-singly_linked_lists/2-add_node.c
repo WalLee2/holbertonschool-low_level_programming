761c7137d8aaffc905c726234fe1cddd9a19a25c
Walton Lee <mr.waltonlee@me.com>
2016-11-29 01:38:44 +0000

    A function that adds a new node at the end of a list_t list.

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 #include <string.h>
 /**
- *
- *
- *
+ *add_node - a function that adds a new node at the beginning of a list_t list
+ *@head: A double pointer that points to the address of what's passed in
+ *@str: The string to be copied by strdup
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;

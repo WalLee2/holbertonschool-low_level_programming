77a5b9c2657e552c8715181782f51ee119a974d0
Walton Lee <mr.waltonlee@me.com>
2016-12-06 09:01:45 +0000

    A function that returns the number of times the bit was flipped to get a number m from n

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ *flip_bits - A function that flips bits until the number n is equal to m
+ *@n: a variable that holds a value
+ *@m: a variable that holds the end result value
+ *Return: The number of times needed to flip the bits in order to change n
+ *to m
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {

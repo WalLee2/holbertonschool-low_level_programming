620e59ed75abf82a88f72a02ccc38782ba55cdba
Walton Lee <mr.waltonlee@me.com>
2016-12-06 03:38:54 +0000

    fixing edge cases and fixing comments

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,19 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *get_bit - a function that uses the index variable to return the value of a bit
+ *get_bit - a function that uses the index
+ *variable to return the value of a bit
  *@n: The decimal value passed
  *@index: The particular bit being observed
+ *Return: the value of the bit at index or -1 if there was an error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	n = n >> index;
 	if (index == 0)
 		return (0);
+	if (index > 64)
+		return (-1);
 	if (n)
 	{
 		n = n % 2;
-		return(n);
+		return (n);
 	}
 	else
 		return (-1);

508924e5d8aa82576ca63bf33d5d55de6c400e92
Walton Lee <mr.waltonlee@me.com>
2016-12-06 04:03:35 +0000

    adjusting conditionals

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -12,11 +12,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	n = n >> index;
 	if (index > 63)
 		return (-1);
-	if (n > 0)
-	{
-		n = n % 2;
-		return (n);
-	}
-	else
-		return (-1);
+	n = n % 2;
+	return (n);
 }

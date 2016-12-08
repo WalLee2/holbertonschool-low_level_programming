9c020cc8dfa0b69f618c1f4729dfa605aa19fdf1
Walton Lee <mr.waltonlee@me.com>
2016-12-08 02:39:41 +0000

    fixing errors

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -18,11 +18,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for(i = 0; text_content[i] != '\0'; i++)
+		for (i = 0; text_content[i] != '\0'; i++)
 			;
-		write (fd, text_content, i);
+		write(fd, text_content, i);
 		return (1);
 	}
-	close (fd);
+	close(fd);
 	return (1);
 }

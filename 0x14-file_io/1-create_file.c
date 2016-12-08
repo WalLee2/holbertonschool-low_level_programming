3da79fcd8b22ca7abd59b14ccfbe19358356be8c
Walton Lee <mr.waltonlee@me.com>
2016-12-08 06:52:52 +0000

    creating a helper function

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -21,8 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
-			;
+		i = strlen_h(text_content);
 		writing = write(fd, text_content, i);
 		if (writing == -1)
 		{
@@ -32,4 +31,14 @@ int create_file(const char *filename, char *text_content)
 	}
 	close(fd);
 	return (1);
+}
+
+int strlen_h(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0' && str != NULL)
+		i++;
+	return (i);
 }

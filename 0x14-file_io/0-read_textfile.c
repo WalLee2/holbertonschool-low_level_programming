48cb18307ba8f2dbb91204d2b99f622a3dfddc56
Walton Lee <mr.waltonlee@me.com>
2016-12-08 01:46:41 +0000

    A function that reads a text file and printing it to standard output.

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *read_textfile - a function that reads a text file and prints it
+ *to the standard output
+ *@filename: a pointer that points to the address of the file
+ *@letters: The number of characters to be printed to std out
+ *Return: All characters of size letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t std_out;
+	int fd;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(letters));
+	if (buf == NULL)
+		return (0);
+	std_out = read(fd, buf, letters);
+	close(fd);
+	printf("%s\n", buf);
+	return (std_out);
+}

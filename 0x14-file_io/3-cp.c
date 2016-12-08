22cee2466ad58d8dface97d354ecc14fcbcadc0d
Walton Lee <mr.waltonlee@me.com>
2016-12-08 22:02:17 +0000

    Accounting for edge cases

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ *main - A function that copies the contents of one file to another
+ *@argc: The number of arguments
+ *@argv: Name of the files being passed
+ *Return: 0 if the function was successful
  */
 int main(int argc, char *argv[])
 {
@@ -17,21 +18,24 @@ int main(int argc, char *argv[])
 		dprintf(STD_ER, "Error: Can't read from file fd %s\n", argv[1]), exit(98);
 	fd_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_write == -1)
-		dprintf(STD_ER, "Error: Can't read from file fd %s\n", argv[1]), exit(98);
+		dprintf(STD_ER, "Error: Can't write to %s\n", argv[2]), exit(98);
 	reading_from = read(fd_read, buf, 1204);
 	if (reading_from == -1)
 		dprintf(STD_ER, "Error: Can't read from file fd %s\n", argv[1]), exit(98);
 	writing_to = write(fd_write, buf, reading_from);
 	if (writing_to == -1)
 		dprintf(STD_ER, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while(reading_from)
+	while (reading_from)
 	{
 		reading_from = read(fd_read, buf, 1204);
 		if (reading_from == -1)
 			dprintf(STD_ER, "Error: Can't read from file fd %s\n", argv[1]), exit(98);
-		writing_to = write(fd_write, buf, reading_from);
-		if (writing_to == -1)
-			dprintf(STD_ER, "Error: Can't write to %s\n", argv[2]), exit(99);
+		if (reading_from > 0)
+		{
+			writing_to = write(fd_write, buf, reading_from);
+			if (writing_to == -1)
+				dprintf(STD_ER, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
 	}
 	close_check = close(fd_read);
 	if (close_check == -1)

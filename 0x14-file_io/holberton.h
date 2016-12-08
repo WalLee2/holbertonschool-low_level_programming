9eebc80ba4fb85fe39608d558d5c17b85a80fff5
Walton Lee <mr.waltonlee@me.com>
2016-12-08 23:42:08 +0000

    fixing error

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#define STD_ER STDERR_FILENO
+#define SERR STDERR_FILENO
 #define BUFFSIZE 1204
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

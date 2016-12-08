3d8366b440b7436db1efd47499c043019e762767
Walton Lee <mr.waltonlee@me.com>
2016-12-08 21:39:13 +0000

    adding the header file

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -3,7 +3,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#define STD_ER STDERR_FILENO
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 #endif

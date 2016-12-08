bff7b0f84ae136870a1996e94e6757553f5dc5c3
Walton Lee <mr.waltonlee@me.com>
2016-12-08 03:14:20 +0000

    changing file permissions

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_des = open(filename, O_WRONLY | O_APPEND, 0600);
+	file_des = open(filename, O_WRONLY | O_APPEND);
 	if (file_des == -1)
 	{
 		close(file_des);

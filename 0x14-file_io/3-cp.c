c352c4a13e4325bf83e7336f6535d77f76c809e2
Walton Lee <mr.waltonlee@me.com>
2016-12-08 22:49:11 +0000

    changing error output

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		dprintf(STD_ER, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_write == -1)
-		dprintf(STD_ER, "Error: Can't write to %s\n", argv[2]), exit(98);
+		dprintf(STD_ER, "Error: Can't write to %s\n", argv[2]), exit(99);
 	reading_from = read(fd_read, buf, 1204);
 	if (reading_from == -1)
 		dprintf(STD_ER, "Error: Can't read from file %s\n", argv[1]), exit(98);

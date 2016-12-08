0a276a002848ad766f173e5d4c9283eabcdb7fba
Walton Lee <mr.waltonlee@me.com>
2016-12-08 23:32:13 +0000

    changing more

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -11,13 +11,14 @@ int main(int argc, char *argv[])
 	char buf[BUFFSIZE];
 	int fd_read, fd_write, close_check;
 	ssize_t reading_from, writing_to;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
 		dprintf(STD_ER, "Usage: cp file_from file_to\n"), exit(97);
 	fd_read = open(argv[1], O_RDONLY);
 	if (fd_read == -1)
 		dprintf(STD_ER, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	fd_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (fd_write == -1)
 		dprintf(STD_ER, "Error: Can't write to %s\n", argv[2]), exit(99);
 	reading_from = 1;
@@ -32,7 +33,7 @@ int main(int argc, char *argv[])
 		if (reading_from > 0)
 		{
 			writing_to = write(fd_write, buf, reading_from);
-			if (writing_to == -1)
+			if (writing_to == -1 || write_to != reading_from)
 				dprintf(STD_ER, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}

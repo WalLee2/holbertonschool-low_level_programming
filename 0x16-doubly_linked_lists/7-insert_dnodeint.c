c03e943f371c30240f119716d942f233ab94339e
Walton Lee <mr.waltonlee@me.com>
2017-01-09 20:47:21 +0000

    finally fixing?

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,31 +1,33 @@
 #include "lists.h"
 /**
  *insert_dnodeint_at_idx - Function that adds a node to the doubly linked list
- *@head: A double pointer holding reference to the beginning of the linked list
+ *@h: A double pointer holding reference to the beginning of the linked list
  *@idx: The index that the node should be added
  *@n: The number to be added to the new node
  *Return: The adjusted doubly linked list
  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *head_b, *head_aft, *new; unsigned int i;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	if (*head == NULL)
+	new->n = n;
+	if (*h == NULL)
 	{
 		if (idx > 0)
 		{
 			free(new); return (NULL);
 		}
-		new->next = NULL; *head = new;
+		*h = new; new->next = NULL; new->prev = NULL;
 		return (new);
 	}
-	new->n = n; new->prev = NULL; head_b = head_aft = *head;
+	new->prev = NULL;
+	head_b = head_aft = *h;
 	if (idx == 0)
 	{
-		head_b->prev = new; new->next = *head; *head = new;
+		head_b->prev = new; new->next = *h; *h = new;
 		return (new);
 	}
 	else if (idx != 0)

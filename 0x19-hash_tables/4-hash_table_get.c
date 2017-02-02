54ad10aaf02d7c57767b6e9ae8eda27c9a68cb99
Walton Lee <mr.waltonlee@me.com>
2017-02-02 03:25:12 +0000

    Retrieving a value associated with a key

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ *hash_table_get - retrieving a value associated with a key.
+ *@ht: Taking in the hash table.
+ *@key: Taking in a character string.
+ *Return: The key if found. NULL otherwise.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx, hash_val;
+
+	hash_val = hash_djb2((unsigned char *)key);
+	idx = hash_val % ht->size;
+	if (ht->array[idx] != NULL)
+	{
+		if (ht->array[idx]->key == key)
+			return (ht->array[idx]->value);
+	}
+	else if (ht->array[idx] == NULL)
+		return (NULL);
+	return (NULL);
+}

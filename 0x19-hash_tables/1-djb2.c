9e3cbf3d2116d1733c2e6eaad0840bbca29853eb
Walton Lee <mr.waltonlee@me.com>
2017-02-02 03:18:54 +0000

    Implementing the djb2 hash function

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ *hash_djb2 - Hash function. Credit: Dan Bernstein
+ *@str: Variable that takes in the key
+ *Return: the hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long hash = 5381;
+	int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}

510230fd35655b9c7b01ff60cf2935fd29ff30d2
Walton Lee <mr.waltonlee@me.com>
2017-02-02 05:10:55 +0000

    error checking for malloc

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,16 +1,18 @@
 #include "hash_tables.h"
 /**
  *hash_table_create - Creating a hash table that contain the key value pairs
- *@size: the size of the space being mallocd
- *Return: The new hash table
+ *@size: the size of the space being mallocd which is a long unsigned int.
+ *Return: The newly created hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *arr_list;
 
 	arr_list = malloc(sizeof(hash_table_t));
-	arr_list->array = malloc(size * sizeof(hash_node_t *));
 	if (arr_list == NULL)
+		return (NULL)
+	arr_list->array = malloc(size * sizeof(hash_node_t *));
+	if (arr_list->array == NULL)
 		return (NULL);
 	arr_list->size = size;
 	return (arr_list);

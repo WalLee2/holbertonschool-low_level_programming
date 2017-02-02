b38e2190e65950fe7475da2e7b4158251792c4e7
Walton Lee <mr.waltonlee@me.com>
2017-02-02 03:15:52 +0000

    updating comments

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,8 +1,9 @@
 #include "hash_tables.h"
 /**
- *
- *
- *
+ *key_index - Getting the index of a key
+ *@key: An unsigned character pointer that takes in a string
+ *@size: Unsigned long integer that takes in a number.
+ *Return: The newly generated index of where the key is going to be set
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {

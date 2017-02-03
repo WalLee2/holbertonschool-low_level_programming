41539650bfd12747486244fcf3c1846a5fd2f7d5
Walton Lee <mr.waltonlee@me.com>
2017-02-03 21:04:51 +0000

    properly accounting for collision

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -9,29 +9,57 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	unsigned long int idx, hash_val, i;
+	unsigned long int idx, hash_val, val_chk;
 
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
 	hash_val = hash_djb2((unsigned char *)key);
 	idx = hash_val % ht->size;
-	for (i = 0; i <= ht->size; i++)
+	if (ht->array[idx] == NULL)
 	{
-		if (ht->array[idx] == NULL)
-		{
-			new->key = (char *)key;
-			new->value = (char *)value;
-			new->next = NULL;
-			ht->array[idx] = new;
-		}
-		else if (strcmp(ht->array[idx]->key, key) == 0)
-		{
-			new->key = (char *)key;
-			new->value = (char *)value;
-			new->next = ht->array[idx];
-			ht->array[idx] = new;
-		}
+		new->key = (char *)key;
+		new->value = (char *)value;
+		new->next = NULL;
+		ht->array[idx] = new;
 	}
+	else
+	{
+		val_chk = collision_chk(ht, idx, (char *)key, (char *)value);
+		if (val_chk == 0)
+			return (0);
+	}
+	return (1);
+}
+/**
+ *collision_chk - Checking if a key exists, if it does change the value
+ *if not, create the node with the new key and value at the top of the list
+ *@ht: Hash table that is being evaluated
+ *@idx: The current idx in the array.
+ *@k: The key of the hash table.
+ *@val: The value associated with the key.
+ *Return: 0 on failure, 1 on success.
+ */
+int collision_chk(hash_table_t *ht, unsigned long int idx, char *k, char *val)
+{
+	hash_node_t *friend, *new_n;
+
+	friend = ht->array[idx];
+	while (friend != NULL)
+	{
+		if (strcmp(friend->key, k) == 0)
+		{
+			ht->array[idx]->value = val;
+			return (1);
+		}
+		friend = friend->next;
+	}
+	new_n = malloc(sizeof(hash_node_t));
+	if (new_n == NULL)
+		return (0);
+	new_n->key = k;
+	new_n->value = val;
+	new_n->next = ht->array[idx];
+	ht->array[idx] = new_n;
 	return (1);
 }

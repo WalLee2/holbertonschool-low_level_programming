03d958407e557c000bb50b4fe3d13dd68e305336
Walton Lee <mr.waltonlee@me.com>
2017-02-23 18:59:37 +0000

    C implementation of insertion sort

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,49 +1,50 @@
 #include "sort.h"
 /**
- *
- *
+ *swap - function that swaps two nodes in a linked doubly linked list
+ *@list: A double pointer pointing to a doubly linked list
+ *@node: A node containing a pointer to prev, next, and an int.
+ *Return: The swapped nodes
+ */
+listint_t *swap(listint_t **list, listint_t *node)
+{
+        listint_t *next, *aft_next;
+
+        next = node->next;
+        aft_next = node->next;
+        while (node != NULL && node->n > next->n)
+        {
+		aft_next = node->next;
+                node->next = next->next;
+		if (node->next != NULL)
+			node->next->prev = node;
+                next->prev = node->prev;
+		if (next->prev != NULL)
+			next->prev->next = next;
+                next->next = node;
+                node->prev = next;
+		node = next->prev;
+		while ((*list)->prev != NULL)
+                        *list = (*list)->prev;
+		print_list(*list);
+	}
+	return (aft_next);
+}
+/**
+ *insertion_sort_list - C implementation of insertion sort
+ *@list - double pointer to a doubly linked list of size 1
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *hlp, *rf_pt;
-	int i;
+	listint_t *helper;
 
-	hlp = *list;
-	i = 0;
-	while (hlp != NULL)
+	helper = *list;
+	if (list == NULL || *list == NULL)
+		return;
+	while (helper != NULL && helper->next != NULL)
 	{
-		rf_pt = hlp->next;
-		while (hlp->prev != NULL && (hlp->n > rf_pt->n))
-			hlp = hlp->prev;
-		if (hlp->n < rf_pt->n)
-		{
-			hlp->next = rf_pt->next;
-			if (rf_pt->next != NULL)
-				rf_pt->next->prev = hlp;
-			rf_pt->next = hlp;
-/*			rf_pt->prev = hlp->prev;*/
-			rf_pt->prev = hlp->prev;
-			if (hlp->prev != NULL)
-				hlp->prev->next = rf_pt;
-			rf_pt->next = hlp;
-			hlp->prev = rf_pt;
-			print_list(*list);
-/*			insertion_sort_list(&rf_pt->prev);*/
-			hlp = hlp->next;
-			i++;
-		}
-/*		else if (hlp->prev == NULL && rf_pt->n > hlp->n)
-		{
-			hlp->prev = rf_pt;
-			rf_pt->next = hlp;
-			rf_pt->prev = NULL;
-			}*/
+		if (helper->n > helper->next->n)
+			swap(list, helper);
 		else
-		{
-			hlp = hlp->next;
-			printf("I'm in the else statement!\n");
-			i++;
-		}
-		printf("Already looped: %d\n", i);
+			helper = helper->next;
 	}
 }

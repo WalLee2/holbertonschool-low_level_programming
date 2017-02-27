9d2858083f5e4842fe9e24de60604eca5ed516ec
Walton Lee <mr.waltonlee@me.com>
2017-02-27 04:03:04 +0000

    adding test files

---
+++ 0x1B-makefiles/main.c
@@ -0,0 +1,18 @@
+#include "m.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 
+ */
+int main(void)
+{
+	print_holberton();
+	return (EXIT_SUCCESS);
+}
+/* Holberton */
+/* Holberton */
+/* Holberton */
+/* Holberton */
+/* Holberton */
+/* Holberton */

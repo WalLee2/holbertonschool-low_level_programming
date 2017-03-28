749e3c54461e35ef4742805d169836ae9c9c860c
Walton Lee <mr.waltonlee@me.com>
2017-03-28 00:24:39 +0000

    Rotating the tree to the right

--- 0x1C-binary_trees/104-binary_tree_rotate_right.c
+++ 0x1C-binary_trees/104-binary_tree_rotate_right.c
@@ -11,14 +11,16 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	temp = NULL;
 	if (tree == NULL)
 		return (NULL);
-	if (tree->left == NULL)
-		return (tree);
 	if (tree->left->right != NULL)
 		temp = tree->left->right;
+	if (tree->left == NULL)
+		return (tree);
 	new_root = tree->left;
 	tree->left->right = tree;
 	tree->parent = tree->left;
 	tree->left->parent = NULL;
 	tree->left = temp;
+	if (temp != NULL)
+		temp->parent = tree;
 	return (new_root);
 }

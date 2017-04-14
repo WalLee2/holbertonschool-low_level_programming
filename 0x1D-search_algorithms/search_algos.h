ef97eebe3b5a00397428602ca73365eb06329313
Walton Lee <mr.waltonlee@me.com>
2017-04-14 23:30:22 +0000

    Using the correct library

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -1,5 +1,6 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
+#include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_me (int *array, size_t size);

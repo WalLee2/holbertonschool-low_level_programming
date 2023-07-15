d7c5ed222e8b28d552fbea3939fec0e9644eb51a
wlee <mr.waltonlee@me.com>
2023-07-14 17:01:01 -0700

    Convert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _atoi - Convert string to int
+ *
+ * @s: input string
+ *
+ * Return: converted integer
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, negative = 0, next = 0, n = 0;
+
+	while (s[i] != '\0')
+	{
+		next = i + 1;
+		if (s[i] == 45 && (s[next] >= 48 && s[next] <= 57))
+		{
+			negative = 1;
+		}
+
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			n += (s[i] - 48);
+			if (s[next] >= 48 && s[next] <= 57)
+			{
+				n *= 10;
+			}
+			else if (s[next] == 32 && s[next + 1])
+			{
+				negative = 0;
+				break;
+			}
+		}
+		i++;
+	}
+	return (negative ? (-1 * n) : n);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void puts2(char *str);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);

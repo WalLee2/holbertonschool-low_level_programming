8b2234d10820affa79668e05d95a6a95e766c80a
wlee <mr.waltonlee@me.com>
2023-07-18 18:07:58 -0700

    Recreate strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - Find the first occurence of the substring
+ *
+ * @haystack: String to search
+ *
+ * @needle: String to match exactly
+ *
+ * Return: pointer to first occurence of substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0, match = 0;
+
+	while (haystack[i] != '\0')
+	{
+		while ((needle[j] == haystack[i]) &&
+		       needle[j] != '\0' && haystack[i] != '\0')
+		{
+			if (!match)
+			{
+				match = i;
+			}
+			i++;
+			j++;
+		}
+
+		if (needle[j] == '\0')
+		{
+			return (haystack + match);
+		}
+		i++;
+	}
+	return (0);
+}

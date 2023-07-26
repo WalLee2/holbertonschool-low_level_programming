d06f3e8114e7354b6e812b869fa6e00452b9b45b
wlee <mr.waltonlee@me.com>
2023-07-26 15:33:00 -0700

    Do not print the null terminator

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
 
+
+/**
+ * clean_up - free the rows if there is an error with malloc
+ *
+ * @grid: grid to free
+ *
+ * @idx: row to free
+ */
+void clean_up(int **grid, int idx)
+{
+	while (idx >= 0)
+	{
+		free(grid[idx]);
+		idx--;
+	}
+	free(grid);
+}
+
+
 /**
  * alloc_grid - create a grid and initilize it to 0
  *
@@ -10,12 +29,18 @@
  *
  * Return: double pointer to the created grid
  */
+
 int **alloc_grid(int width, int height)
 {
 	int i, j, **grid;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
 	grid = malloc(height * sizeof(int));
-	if (grid == NULL || width == 0 || height == 0)
+	if (grid == NULL)
 	{
 		return (NULL);
 	}
@@ -23,8 +48,10 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
+
 		if (grid[i] == NULL)
 		{
+			clean_up(grid, i);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)

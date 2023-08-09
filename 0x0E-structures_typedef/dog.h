91ab9a0bd1415ea5614693d7b6aa9741addbc959
wlee <mr.waltonlee@me.com>
2023-08-08 20:15:15 -0700

    Changed struct dog to dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  *@owner: Char pointer to a string owner
  *@age: float that represents the age of the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
